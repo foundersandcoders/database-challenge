bf4713b477a63d88cb087c29d4eb1533cbfe55ec
oliverjam <oliverjamesphillips@gmail.com>
2022-09-21 12:24:45 +0200

    Fix paths to DB in solution

--- solution/database/seed.js
+++ solution/database/seed.js
@@ -2,12 +2,8 @@ const { readFileSync } = require("node:fs");
 const { join } = require("node:path");
 const db = require("./db.js");
 
-/**
- * Run this in development if you need to recreate your DB.
- * It'll add all the example values from seed.sql.
- */
-
-const seedPath = join("database", "seed.sql");
+const seedPath = join("src", "database", "seed.sql");
 const seed = readFileSync(seedPath, "utf-8");
 db.exec(seed);
+
 console.log("db.sqlite seeded with example data");
