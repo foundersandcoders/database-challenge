6c86e2754cb9a091d86ee3ddf40438967dd8a8a4
oliverjam <oliverjamesphillips@gmail.com>
2022-09-21 12:12:14 +0200

    Fix paths to DB files

--- src/database/db.js
+++ src/database/db.js
@@ -2,17 +2,9 @@ const { readFileSync } = require("node:fs");
 const { join } = require("node:path");
 const Database = require("better-sqlite3");
 
-/**
- * Create a new DB file, or use an existing one
- */
-const db = new Database("db.sqlite");
+const db = new Database(process.env.DB_FILE);
 
-/**
- * Make sure DB has the right structure by running schema.sql
- * This is responsible for creating the tables and columns we need
- * It should be safe to run every time (e.g. using `CREATE IF NOT EXISTS`)
- */
-const schemaPath = join("database", "schema.sql");
+const schemaPath = join("src", "database", "schema.sql");
 const schema = readFileSync(schemaPath, "utf-8");
 db.exec(schema);
 
